5121e3aa2871c3a2cdd940def8130477de05481c
Kris <93468625+Ohramario@users.noreply.github.com>
2023-08-29 13:41:52 +0300

    added array for user input, and now user input is shown in web page

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,6 +1,28 @@
 let number1;
 let number2;
 let operator;
+let valuesArray = [];
+let displayValues = " ";
+
+//add event listener to buttons
+const buttons = document.querySelectorAll('button');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        //add user input intto an array
+        valuesArray.push(button.id);
+        //display array (input of user)
+        document.querySelector("#display").textContent = valuesArray.join(" ");
+
+    });
+});
+
+
+
+
+
+
 
 function add(n1, n2) {
     return n1 + n2;
@@ -23,17 +45,13 @@ function operate(number1, number2, operator) {
             multiply(number1, number2);
             break;
         case "substract":
-        subtract(number1, number2);
+            subtract(number1, number2);
             break;
-
-        case divide(number1, number2){
+        case "divide":
             divide(number1, number2);
             break;
-        }
-        default: 
-        console.log("sorry operator not available")
-
+        default:
+            console.log("sorry operator not available")
     }
 }
 
-console.log(subtract(1, 2));
