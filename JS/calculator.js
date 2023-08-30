abe061d4f1aa3d47d95ee1197f1da9513f66412a
Kris <93468625+Ohramario@users.noreply.github.com>
2023-08-30 15:53:07 +0300

    fixed the clear function

--- JS/calculator.js
+++ JS/calculator.js
@@ -3,19 +3,15 @@ let number2;
 let operator;
 let valuesArray = [];
 
-
-
-
-
 //add event listener to buttons
 const buttons = document.querySelectorAll('button');
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
-        //add user input into an array
+        //add user input into an array, exluding clear
         valuesArray.push(check(button.id));
-        //resets values
+        //checks what needs to be done
         findAction(button.id);
     });
 });
@@ -24,6 +20,7 @@ function findAction(action) {
     switch (action) {
         case "clear":
             valuesArray = [];
+            clearDisplay();
             break;
         case "=":
             operate(number1, number2, action)
@@ -34,14 +31,9 @@ function findAction(action) {
             //display array (input of user)
             document.querySelector("#display").textContent = valuesArray.join(" ");
     }
-
-
-
 }
 
 function findOperator(array) {
-    //convert Array into numbers
-
     //find the operator
     valuesArray.forEach((number) => {
         if (isNaN(number)) {
@@ -53,19 +45,21 @@ function findOperator(array) {
 }
 
 function getNum1(index) {
-
     let newNumber = valuesArray.slice(0, index).join("");
     return Number(newNumber);
-
+}
+function clearDisplay() {
+    document.querySelector("#display").textContent = "";
 }
 
-function clearDisplay() {
+/*function clearDisplay() {
     const clear = document.getElementById("clear");
     clear.addEventListener("click", () => {
-        document.getElementById("MyDiv").innerHTML = "";
+        document.querySelector("#display").textContent = "";
 
     })
 }
+*/
 //checks for clear
 function check(input) {
     if (input != "clear") {
@@ -84,19 +78,24 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
     return n1 / n2;
 }
+
+function showResult(number) {
+    document.getElementById("display").innerHTML = number;
+}
+
 function operate(number1, number2, operator) {
     switch (operator) {
-        case "add":
-            add(number1, number2);
+        case "+":
+            showResult(add(number1, number2));
             break;
-        case "multiply":
-            multiply(number1, number2);
+        case "*":
+            showResult(multiply(number1, number2));
             break;
-        case "substract":
-            subtract(number1, number2);
+        case "-":
+            showResult(subtract(number1, number2));
             break;
-        case "divide":
-            divide(number1, number2);
+        case "/":
+            showResult(divide(number1, number2));
             break;
         default:
             console.log("sorry operator not available")
